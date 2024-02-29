f8e644eb49134abe74695d7ada672cff687c8eda
gabriellafabiao <87137798+gabriellafabiao@users.noreply.github.com>
2024-02-28 23:41:45 -0300

    Add files via upload

--- stella-diamonds/js/script.js
+++ stella-diamonds/js/script.js
@@ -1,9 +1,4 @@
-// Selecionar todos os elementos de vídeo com a classe 'meuVideo'
 var videos = document.querySelectorAll(".meuVideo");
-
-// Iterar sobre cada vídeo e aplicar o comportamento desejado
 videos.forEach(function(video) {
-  // Iniciar o vídeo automaticamente quando a página carregar
   video.autoplay = true;
 });
-
